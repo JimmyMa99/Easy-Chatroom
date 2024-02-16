b25c8309285cc0e2777114caf4f93fb3f1eef622
JimmyMa99 <mazhiming312@outlook.com>
2024-02-17 02:58:51 +0800

    add openai-chatbot login

---
+++ db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect('chatroom.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users
+                 (username TEXT PRIMARY KEY, password TEXT)''')
+    conn.commit()
+    conn.close()
+
+create_db()
